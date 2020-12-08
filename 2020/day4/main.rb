6a219ca20560249bad005824e02843ab666b289b
sreedev <sreedevpadmakumar@gmail.com>
2020-12-08 09:12:23 +0530

    day4 solution

--- 2020/day4/main.rb
+++ 2020/day4/main.rb
@@ -1,14 +1,5 @@
 required_fields = %w(byr iyr eyr hgt hcl ecl pid) # cid is optional
 
-# byr (Birth Year) - four digits; at least 1920 and at most 2002.
-# iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-# eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-# hgt (Height) - a number followed by either cm or in: If cm, the number must be at least 150 and at most 193.  If in, the number must be at least 59 and at most 76.
-# hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-# ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-# pid (Passport ID) - a nine-digit number, including leading zeroes.
-# cid (Country ID) - ignored, missing or not.
-
 raw_passport_data = File.open('./data.txt', 'r').read
 processed_passports = raw_passport_data.split("\n\n")
 
@@ -40,5 +31,5 @@ valid_passports_2 = valid_passports_1.select do |passport|
     passport['hcl'].to_s.match(/^#[0-9a-f]{6}$/)
 end
 
-puts "valid_passports(1): #{valid_passports_1.count}"
-puts "valid_passports(2): #{valid_passports_2.count}"
+puts "valid_passports(part1): #{valid_passports_1.count}"
+puts "valid_passports(part2): #{valid_passports_2.count}"

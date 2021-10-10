a22196534fd670447794952be5ee356ff0ae14df
sreedev <sreedevpadmakumar@gmail.com>
2021-10-11 01:34:59 +0530

    index structure

--- README.md
+++
@@ -1,3 +0,0 @@
-# Competitive Programming Solutions
-
-A compilation of all competitive programming solutions that I've attempted in multiple languages
--- advent_of_code/ruby/2015/day5/solution/test/solution_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule SolutionTest do
-  use ExUnit.Case
-  doctest Solution
-
-  test "greets the world" do
-    assert Solution.hello() == :world
-  end
-end

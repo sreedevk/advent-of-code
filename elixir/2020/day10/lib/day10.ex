34afe4a23146b2731e9438716e1f31730549ea2a
sreedev <sreedevpadmakumar@gmail.com>
2021-11-14 10:06:53 +0530

    elixir: 2020 day 10

--- elixir/2020/day10/lib/day10.ex
+++ elixir/2020/day10/lib/day10.ex
@@ -1,18 +1,32 @@
 defmodule Day10 do
-  @moduledoc """
-  Documentation for `Day10`.
-  """
+  def solve(1) do
+    data()
+    |> adapters_list()
+    |> sum_of_joltage_differences()
+    |> Enum.group_by(&(&1))
+    |> Map.values()
+    |> Enum.map(&Enum.count/1)
+    |> Enum.reduce(&(&1*&2))
+  end
 
-  @doc """
-  Hello world.
+  def solve(2), do: false
 
-  ## Examples
+  defp sum_of_joltage_differences(adapters) do
+    adapters
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(fn [output_adapter, input_adapter] ->
+      (input_adapter - output_adapter)
+    end)
+  end
 
-      iex> Day10.hello()
-      :world
+  defp adapters_list(raw_data) do
+    [Enum.sum([Enum.max(raw_data), 3]) | raw_data]
+    |> Enum.sort()
+  end
 
-  """
-  def hello do
-    :world
+  defp data do
+    File.read!("data.txt")
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
   end
 end

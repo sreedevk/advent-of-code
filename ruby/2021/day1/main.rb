cebc685c6ee112fe7e07a8c980e37daad09f1595
sreedev <sreedevpadmakumar@gmail.com>
2021-12-01 13:34:16 +0530

    added ruby shorthand for block variables

--- ruby/2021/day1/main.rb
+++ ruby/2021/day1/main.rb
@@ -4,11 +4,11 @@ class SonarSweep
   end
 
   def solve_1
-    data.each_cons(2).count {|x, y| y > x }
+    data.each_cons(2).count { _2 > _1 }
   end
 
   def solve_2
-    data.map(&:to_i).each_cons(3).map(&:sum).each_cons(2).count {|x, y| y > x }
+    data.map(&:to_i).each_cons(3).map(&:sum).each_cons(2).count { _2 > _1 }
   end
 end
 

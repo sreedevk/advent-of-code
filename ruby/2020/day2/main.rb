2fb62f92b8cb7d487a4791204c194a741ecb1b61
sreedev <sreedevpadmakumar@gmail.com>
2021-12-02 14:23:03 +0530

    day 2 2020 optimized

--- ruby/2020/day2/main.rb
+++ ruby/2020/day2/main.rb
@@ -1,36 +1,32 @@
 # frozen_string_literal: true
 
-def password_info_list
-  File.open('./data.txt', 'r').readlines
-end
-
-def part_one_valid?(password, policy_char, range_start, range_end)
-  policy_char_count_in_password = password.count(policy_char)
-  policy_char_count_in_password >= range_start && policy_char_count_in_password <= range_end
-end
-
-def part_two_valid?(password, policy_char, first_position, second_position)
-  [password[first_position.pred] == policy_char, password[second_position.pred] == policy_char].uniq.length > 1
-end
-
-def count_valid_passwords(password_policy)
-  valid_passwords = 0
-  password_info_list.each do |password_info|
-    policy, password = password_info.split(':').map(&:strip)
-    policy_char_range, policy_char = policy.split(' ')
-    policy_char_range_start, policy_char_range_end = policy_char_range.split('-').map(&:to_i)
-    valid_passwords += 1 if password_policy.call(password, policy_char, policy_char_range_start, policy_char_range_end)
+class PasswordPhilosophy
+  def data
+    @data ||= ARGF.readlines
   end
-  valid_passwords
+
+  def part1
+    data
+      .map { _1.match(/^(?<minim>\d+)-(?<maxim>\d+) (?<char>[a-z]): (?<password>\w+)/).named_captures }
+      .count { Range.new(_1["minim"].to_i, _1["maxim"].to_i).cover?(_1["password"].count(_1["char"]))  }
+  end
+
+  def part2
+    data
+      .map { _1.match(/^(?<alphapos>\d+)-(?<betapos>\d+) (?<char>[a-z]): (?<password>\w+)/).named_captures }
+      .count do
+        _1["password"]
+          .chars
+          .values_at(_1["alphapos"].to_i.pred, _1["betapos"].to_i.pred)
+          .compact
+          .tally
+          .send(:[], _1["char"])
+          .eql?(1)
+      end
+  end
 end
 
-def part1
-  count_valid_passwords(self.method(:part_one_valid?))
-end
+solver = PasswordPhilosophy.new
 
-def part2
-  count_valid_passwords(self.method(:part_two_valid?))
-end
-
-puts "part1 solution: #{part1}"
-puts "part2 solution: #{part2}"
+puts "PART I: #{solver.part1}"
+puts "PART II: #{solver.part2}"

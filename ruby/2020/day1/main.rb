007c91cdffe593f78ca4e899b308af654aae1d20
sreedev <sreedevpadmakumar@gmail.com>
2021-12-02 13:36:48 +0530

    optimized day1 2020

--- ruby/2020/day1/main.rb
+++ ruby/2020/day1/main.rb
@@ -1,36 +1,22 @@
 # frozen_string_literal: true
 
-def data
-  File.open('./data.txt', 'r').readlines.map(&:strip).map(&:to_i)
-end
+class ReportRepair
+  REQUIRED_SUM = 2020
 
-REQUIRED_SUM = 2020
-
-def numbers_less_than_required_sum
-  data.select { |number| number < REQUIRED_SUM }
-end
-
-def part1
-  sum_pair =  []
-  numbers_less_than_required_sum.map do |input_one|
-    numbers_less_than_required_sum.map do |input_two|
-      sum_pair = [input_one, input_two] if input_one + input_two == REQUIRED_SUM
-    end
+  def data
+    @data ||= ARGF.readlines.map(&:strip).map(&:to_i)
   end
-  sum_pair.inject(:*)
-end
-
-def part2
-  sum_triplet = []
-  numbers_less_than_required_sum.map do |input_one|
-    numbers_less_than_required_sum.map do |input_two|
-      numbers_less_than_required_sum.map do |input_three|
-        sum_triplet = [input_one, input_two, input_three] if input_one + input_two + input_three == REQUIRED_SUM
-      end
-    end
+  
+  def part1
+    data.combination(2).find { |x, y| x + y == REQUIRED_SUM }.inject(:*)
   end
-  sum_triplet.inject(:*)
+
+  def part2
+    data.combination(3).find { |x, y, z| x + y + z == REQUIRED_SUM }.inject(:*)
+  end
 end
 
-puts "part1 solution: #{part1}"
-puts "part2 solution: #{part2}"
+solver = ReportRepair.new
+
+puts "part I: #{solver.part1}"
+puts "part II: #{solver.part2}"

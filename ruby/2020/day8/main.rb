abef13987f8f4f98c859eea77e74ddb4fb20af67
sreedev <sreedevpadmakumar@gmail.com>
2021-12-03 06:17:26 +0530

    optimized day8 solution

--- ruby/2020/day8/main.rb
+++ ruby/2020/day8/main.rb
@@ -1,84 +1,73 @@
 require 'pry'
 
-$accumulator = 0
-$stack_ptr = 0
-$stack_ptr_history = []
-
-def opcodes
-  File.open('./data.txt', 'r').readlines
-  # File.open('./example.txt', 'r').readlines
-end
-
-def parse_instructions(opcode_list)
-  opcode_list.map do |iseq|
-    inst, arg = iseq.split(' ')
-    [inst, arg.to_i]
+class HandheldHalting
+  def data
+    @data ||= ARGF.readlines
+      .map { _1.split(" ") .map(&:strip) }
+      .map { [_1.to_sym, Integer(_2)] }
   end
-end
 
-def acc(iseq_arg)
-  $accumulator += iseq_arg
-  $stack_ptr_history << $stack_ptr
-  $stack_ptr += 1
-end
-
-def jmp(iseq_arg)
-  $stack_ptr_history << $stack_ptr
-  $stack_ptr += iseq_arg
-end
-
-def nop(iseq_arg)
-  $stack_ptr_history << $stack_ptr
-  $stack_ptr += 1
-end
-
-def run(iseq)
-  self.send(iseq[$stack_ptr][0].to_sym, iseq[$stack_ptr][1])
-  return false if $stack_ptr_history.include?($stack_ptr)
-
-  ($stack_ptr < iseq.length) ? run(iseq) : true
-end
-
-def reset_machine
-  $accumulator = 0
-  $stack_ptr = 0
-  $stack_ptr_history = []
-end
-
-def alter_sector(seq, index)
-  seq[index][0] = (seq[index][0] == 'jmp' ? 'nop' : 'jmp')
-  seq
-end
-
-def part1
-  reset_machine
-  boot_seq = parse_instructions(opcodes)
-  run(boot_seq)
-  puts "last value of accumulator(part1): #{$accumulator}"
-end
-
-def part2
-  boot_seq = parse_instructions(opcodes).freeze
-  counter = 0
-  terminated = false
-  while(!terminated)
-    break if !boot_seq[counter]
-
-    cloned_seq = parse_instructions(opcodes)
-    unless ['jmp', 'nop'].include?(cloned_seq[counter][0])
-      counter += 1
-      next
+  def part1(state = initial_state, instructions = data.clone)
+    if (state[:looped] = state[:ptr_history].include?(state[:ptr])) ||
+      (state[:ptr] > instructions.length.pred)
+      return state 
     end
 
-    cloned_seq = alter_sector(cloned_seq, counter)
-    reset_machine
-    terminated = run(cloned_seq)
-    puts "altered_sector: #{cloned_seq[counter]}; original_sector: #{boot_seq[counter]}; terminated? #{terminated}"
-    break if terminated
-    counter += 1
+    part1(self.send(*instructions[state[:ptr]], state))
   end
-  puts "last value of accumulator(part2): #{$accumulator}"
+
+  def part2(state = initial_state)
+    variable_instruction_locations = data
+      .map
+      .with_index
+      .select { [:jmp, :nop].include?(_1[0][0]) }
+      .map { _1[1] }
+
+    variable_instruction_locations
+      .map { |instr_index| [instr_index, part1(initial_state, switched_instructions(instr_index)) ] }
+      .find { !_1[1][:looped] }
+  end
+
+  private
+
+  def initial_state
+    {ptr_history: [], ptr: 0, accumulator: 0, looped: false}
+  end
+
+  def switched_instructions(index_to_switch_at)
+    instructions = data.clone
+    instructions[index_to_switch_at] = [
+      [:nop, :jmp].find { instructions[index_to_switch_at][0] != _1 },
+      instructions[index_to_switch_at][1]
+    ]
+    instructions
+  end
+
+  def jmp(arg, state)
+    {
+      ptr_history: state[:ptr_history] + [state[:ptr]],
+      ptr: state[:ptr] + arg,
+      accumulator: state[:accumulator]
+    }
+  end
+
+  def nop(arg, state)
+    {
+      ptr_history: state[:ptr_history] + [state[:ptr]],
+      ptr: state[:ptr] + 1,
+      accumulator: state[:accumulator]
+    }
+  end
+
+  def acc(arg, state)
+    {
+      ptr_history: state[:ptr_history] + [state[:ptr]],
+      ptr: state[:ptr] + 1,
+      accumulator: state[:accumulator] + arg
+    }
+  end
 end
 
-part1
-part2
+solver = HandheldHalting.new
+puts "part I: #{solver.part1[:accumulator]}"
+puts "part II: #{solver.part2[:accumulator]}"

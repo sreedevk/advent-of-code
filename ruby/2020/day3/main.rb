9ae0539624e0b71e85ccf5aeb13f4487cee46485
sreedev <sreedevpadmakumar@gmail.com>
2021-12-03 07:15:20 +0530

    optimized day2020 day3

--- ruby/2020/day3/main.rb
+++ ruby/2020/day3/main.rb
@@ -1,47 +1,46 @@
 # frozen_string_literal: true
 
-def geomap
-  File.open('./data.txt', 'r').readlines.map(&:chars)
-end
+require 'pry'
 
-def traverse(slope, terrain, current_x, current_y, tree_count)
-  slope[0].times do
-    if current_x == 30
-      (current_x = 0)
-    else
-      current_x += 1
-    end
+class TobogganTrajectory
+  X = :x; Y = :y
+
+  def terrain
+    @terrain ||= ARGF.readlines.map(&:strip).map(&:chars)
   end
-  current_y += slope[1]
-  tree_count += 1 if current_y < terrain.length && terrain.dig(current_y, current_x) == '#'
-  [current_x, current_y, tree_count]
-end
 
-def part1
-  slope = [3, 1]
-  current_x, current_y, tree_count = 0, 0, 0
-  terrain = geomap
-  terrain.each do
-    break if current_y >= terrain.length
+  def traverse(path_pattern, state={x: 0, y: 0, trees: 0})
+    updated_state = {
+      x: (state[X] + path_pattern[0]) % max_x,
+      y: state[Y] + path_pattern[1],
+      trees: tree?(terrain.dig(state[Y], state[X])) ? (state[:trees] + 1) : state[:trees]
+    }
+    return updated_state unless terrain.dig(updated_state[Y])
 
-    current_x, current_y, tree_count = traverse(slope, terrain, current_x, current_y, tree_count)
+    traverse(path_pattern, updated_state)
   end
-  tree_count
+
+  def part1
+    traverse([3, 1])[:trees]
+  end
+
+  def part2
+    [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+      .map { traverse(_1)[:trees] }
+      .inject(:*)
+  end
+
+  private
+
+  def max_x
+    terrain[0].length
+  end
+
+  def tree?(value)
+    value == '#'
+  end
 end
 
-def part2
-  slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
-  slopes.map do |slope|
-    current_x, current_y, tree_count = 0, 0, 0
-    terrain = geomap
-    terrain.each do
-      break if current_y >= terrain.length
-
-      current_x, current_y, tree_count = traverse(slope, terrain, current_x, current_y, tree_count)
-    end
-    tree_count
-  end.inject(:*)
-end
-
-puts "part1 solution: #{part1}"
-puts "part2 solution: #{part2}"
+solver = TobogganTrajectory.new
+puts "PART I: #{ solver.part1 }"
+puts "PART II: #{ solver.part2 }"

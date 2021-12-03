fd8239ef8c597607b71af1bdfe324b3262b6dddb
sreedev <sreedevpadmakumar@gmail.com>
2021-12-03 22:04:20 +0530

    submitted day 3 2021 - Ruby - Part 1 & Elixir Part 2

---
+++ elixir/2021/day03/lib/day03.ex
@@ -0,0 +1,38 @@
+defmodule Day03 do
+  def solve(1) do
+    Enum.reduce([gamma_rate(data()), epsilon_rate(data())], &(&1 * &2))
+  end
+
+  def solve(2), do: "Solved in Ruby"
+
+  defp gamma_rate(dataset) do
+    Range.new(0, length(Enum.fetch!(dataset, 0)) - 1)
+    |> Enum.map(fn position -> Enum.map(dataset, &Enum.fetch!(&1, position)) end)
+    |> Enum.map(fn position_set -> Enum.frequencies(position_set) end)
+    |> Enum.map(fn position_freq ->
+      Enum.max_by(Enum.to_list(position_freq), fn freq_list -> List.last(Tuple.to_list(freq_list)) end)
+    end)
+    |> Enum.map(fn { max_digit, _ } -> max_digit end)
+    |> Enum.join()
+    |> String.to_integer(2)
+  end
+
+  defp epsilon_rate(dataset) do
+    Range.new(0, length(Enum.fetch!(dataset, 0)) - 1)
+    |> Enum.map(fn position -> Enum.map(dataset, &Enum.fetch!(&1, position)) end)
+    |> Enum.map(fn position_set -> Enum.frequencies(position_set) end)
+    |> Enum.map(fn position_freq ->
+      Enum.min_by(Enum.to_list(position_freq), fn freq_list -> List.last(Tuple.to_list(freq_list)) end)
+    end)
+    |> Enum.map(fn { max_digit, _ } -> max_digit end)
+    |> Enum.join()
+    |> String.to_integer(2)
+  end
+
+  defp data do
+    File.read!("data.txt")
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.graphemes/1)
+    |> Enum.map(fn number -> Enum.map(number, &String.to_integer/1) end)
+  end
+end
---
+++ ruby/2021/day3/main.rb
@@ -0,0 +1,47 @@
+require 'matrix'
+
+class BinaryDiagnostic
+  def data
+    @data ||= ARGF.readlines.map(&:strip).map(&:chars).map { _1.map(&:to_i) }
+  end
+
+  def part2
+    filter_oxygen(data).join.to_i(2) * filter_carbon(data).join.to_i(2)
+  end
+
+  def gamma_rate(numbers)
+    Matrix[numbers]
+  end
+
+  def filter_oxygen(numbers, column_index = 0)
+    mode_bit       = oxy_mode_bit_calc(Matrix[*numbers].column(column_index).to_a.tally.invert)
+    filtered_layer = numbers.select { |number| number[column_index] == mode_bit }
+    return filtered_layer[0] if filtered_layer.length < 2
+
+    filter_oxygen(filtered_layer, column_index + 1)
+  end
+
+  def filter_carbon(numbers, column_index = 0)
+    mode_bit = carbox_mode_bit_calc(Matrix[*numbers].column(column_index).to_a.tally.invert)
+    filtered_layer = numbers.select { |number| number[column_index] == mode_bit }
+    return filtered_layer[0] if filtered_layer.length < 2
+
+    filter_carbon(filtered_layer, column_index + 1)
+  end
+
+  def carbox_mode_bit_calc(freq_dist)
+    return 0 unless freq_dist.keys.uniq.length > 1
+
+    freq_dist[freq_dist.keys.min]
+  end
+
+  def oxy_mode_bit_calc(freq_dist)
+    return 1 unless freq_dist.keys.uniq.length > 1
+
+    freq_dist[freq_dist.keys.max]
+  end
+end
+
+solver = BinaryDiagnostic.new
+puts "PART I: SOLVED IN ELIXIR"
+puts "PART II: #{solver.part2.inspect}"

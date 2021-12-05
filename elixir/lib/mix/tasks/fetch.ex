e1ff3cbf46d5b9ad207b62b476fc5c67cfeabc84
sreedev <sreedevpadmakumar@gmail.com>
2021-12-05 07:00:49 +0530

    mix helper added to fetch problem input

--- elixir/lib/mix/tasks/fetch.ex
+++ elixir/lib/mix/tasks/fetch.ex
@@ -3,9 +3,18 @@ defmodule Mix.Tasks.Fetch do
 
   @impl Mix.Task
   def run(["authenticate", cookie]) do
+    File.write!(".cookie", cookie)
   end
 
   @impl Mix.Task
   def run(["problem", year, day]) do
+    File.mkdir_p!("data/aoc/twenty#{year}/day#{day}/")
+    HTTPoison.start()
+    File.write!(
+      "data/aoc/twenty#{year}/day#{day}/data.txt", 
+      HTTPoison.get!("https://adventofcode.com/20#{year}/day/#{day}/input", [
+        Cookie: "session=#{File.read!(".cookie")}"
+      ]).body
+    )
   end
 end

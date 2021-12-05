19d0d7686cab0d93b11317f1f625d87d3af36739
sreedev <sreedevpadmakumar@gmail.com>
2021-12-06 02:24:17 +0530

    adding point-free optimizations

--- ruby/2021/day5/main.rb
+++ ruby/2021/day5/main.rb
@@ -1,40 +1,51 @@
 require 'pry'
+
 class HydrothermalVenture
   attr_accessor :grid
 
-  def initialize
-    @grid = Hash.new(0)
+  def part1(veins = Hash.new(0))
+    veins = data
+      .filter { |line| line.reduce(&:zip).any? { _1.minmax.reduce(&:eql?) } }
+      .reduce(Hash.new) { |grid, line|
+        grid.merge(
+          line
+            .reduce(&:zip)
+            .map { Range.new(*_1.sort).to_a }
+            .reduce(&:product)
+            .reduce(Hash.new(0)) { _1.merge(Hash[_2, 1]) }
+        ) { _2 + _3 }
+      }
+
+    [veins, veins.count { _2 >= 2 }]
   end
 
-  def part1
-    data
-      .filter { |line| line[0].zip(line[1]).any? { _1.min == _1.max } }
-      .reduce(@grid) { |grid, line|
-        xs, ys = line[0].zip(line[1]).map(&:sort).map { Range.new(_1, _2).to_a }
-        xs.product(ys).map { grid[_1] += 1 }
-        grid
-      }
-      .count { _2 >= 2 }
+  def part2(veins = Hash.new(0))
+    veins = veins.merge(
+      data
+        .reject { |line| line.reduce(&:zip).any? { _1.minmax.reduce(&:eql?) } }
+        .reduce(veins) { |grid, line|
+          grid.merge(
+            line.reduce(&:zip).map { [_1, _2, (_2 - _1).negative? ? -1 : 1] }
+              .map { |p1, p2, dp| (dp.negative? ? [p1, p2].max.step([p1, p2].min, dp) : [p1, p2].min.step([p1, p2].max, dp)).to_a }
+              .reduce(&:zip)
+              .reduce(Hash.new(0)) { _1.merge(Hash[_2, 1]) }
+          ) { _2 + _3 }
+        }
+    )
+
+    [veins, veins.count { _2 >= 2 }]
   end
 
-  def part2
-    data
-      .reject { |line| line[0].zip(line[1]).any? { _1.min == _1.max } }
-      .reduce(@grid) { |grid, line|
-        xs, ys         = line[0].zip(line[1])
-        dx, dy         = [xs, ys].map { (_2 - _1).negative? ? -1 : 1  }
-        xrange, yrange = [[xs, dx], [ys, dy]].map { |ps, dp| (dp.negative? ? ps.max.step(ps.min, dp) : ps.min.step(ps.max, dp)).to_a }
-        xrange.zip(yrange).map {  grid[ _1 ] += 1 }
-        grid
-      }
-      .count { _2 >= 2}
+  def solve
+    veins, p1solve  = part1
+    _veins, p2solve = part2(veins)
+    [p1solve, p2solve]
   end
 
   def data
-    @data ||= ARGF.readlines.map { _1.split(" -> ").map { |point| point.split(",").map(&:strip).map(&:to_i) } } 
+    @data ||= ARGF.readlines.map { _1.scan(/\d+/).map(&:strip).map(&:to_i).each_slice(2).to_a } 
   end
 end
 
 solver = HydrothermalVenture.new
-pp solver.part1
-pp solver.part2
+pp solver.solve

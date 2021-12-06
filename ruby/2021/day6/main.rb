f03040bf03ccec4f9dc1a01890904246adc8b9b8
sreedev <sreedevpadmakumar@gmail.com>
2021-12-07 02:22:22 +0530

    solution optimization ruby - day 6 2021

--- ruby/2021/day6/main.rb
+++ ruby/2021/day6/main.rb
@@ -13,8 +13,7 @@ class Lanternfish
 
   def emulate(fishes, days, day = 1)
     next_generation    = Hash.new(0)
-    next_generation[8] = fishes[0].to_i
-    next_generation[6] = fishes[0].to_i
+    next_generation.merge!({ 8 => fishes[0].to_i, 6 => fishes[0].to_i })
     next_generation.merge!(fishes.select{ _1.positive? }.transform_keys { _1 - 1 }) { _2 + _3 }
     return next_generation if day >= days
 

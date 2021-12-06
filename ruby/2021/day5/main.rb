28e18ddba7cc4a7ab2b465ffc7b12d6b6e77e06e
sreedev <sreedevpadmakumar@gmail.com>
2021-12-06 13:08:36 +0530

    opmizations for 2021 day5 - ruby

--- ruby/2021/day5/main.rb
+++ ruby/2021/day5/main.rb
@@ -1,12 +1,8 @@
-require 'pry'
-
 class HydrothermalVenture
-  attr_accessor :grid
-
-  def part1(veins = Hash.new(0))
-    veins = data
+  def part1(input)
+    input
       .filter { |line| line.reduce(&:zip).any? { _1.minmax.reduce(&:eql?) } }
-      .reduce(Hash.new) { |grid, line|
+      .reduce(Hash.new(0)) { |grid, line|
         grid.merge(
           line
             .reduce(&:zip)
@@ -15,31 +11,30 @@ class HydrothermalVenture
             .reduce(Hash.new(0)) { _1.merge(Hash[_2, 1]) }
         ) { _2 + _3 }
       }
-
-    [veins, veins.count { _2 >= 2 }]
   end
 
-  def part2(veins = Hash.new(0))
-    veins = veins.merge(
-      data
-        .reject { |line| line.reduce(&:zip).any? { _1.minmax.reduce(&:eql?) } }
-        .reduce(veins) { |grid, line|
-          grid.merge(
-            line.reduce(&:zip).map { [_1, _2, (_2 - _1).negative? ? -1 : 1] }
-              .map { |p1, p2, dp| (dp.negative? ? [p1, p2].max.step([p1, p2].min, dp) : [p1, p2].min.step([p1, p2].max, dp)).to_a }
-              .reduce(&:zip)
-              .reduce(Hash.new(0)) { _1.merge(Hash[_2, 1]) }
-          ) { _2 + _3 }
-        }
-    )
-
-    [veins, veins.count { _2 >= 2 }]
+  def part2(input)
+    input
+      .reject { |line| line.reduce(&:zip).any? { _1.minmax.reduce(&:eql?) } }
+      .reduce(Hash.new(0)) { |grid, line|
+        grid.merge(
+          line.reduce(&:zip).map { [_1, _2, (_2 - _1).negative? ? -1 : 1] }
+            .map { |p1, p2, dp| (dp.negative? ? [p1, p2].max.step([p1, p2].min, dp) : [p1, p2].min.step([p1, p2].max, dp)).to_a }
+            .reduce(&:zip)
+            .reduce(Hash.new(0)) { _1.merge(Hash[_2, 1]) }
+        ) { _2 + _3 }
+      }
   end
 
   def solve
-    veins, p1solve  = part1
-    _veins, p2solve = part2(veins)
-    [p1solve, p2solve]
+    input = data.clone.freeze
+    p1veins = part1(input)
+    p2veins = part2(input)
+
+    [
+      p1veins.count { _2 >= 2 },
+      p1veins.merge(p2veins) { _2 + _3 }.count { _2 >= 2}
+    ]
   end
 
   def data

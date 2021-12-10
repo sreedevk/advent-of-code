0197b8ee48bc7496a4a258caa3b8599734d43bc8
sreedev <sreedevpadmakumar@gmail.com>
2021-12-11 03:53:22 +0530

    day 10 2021 ruby solution

--- ruby/2021/day10/main.rb
+++ ruby/2021/day10/main.rb
@@ -1,5 +1,51 @@
-    class SyntaxScoring
-      def data
-        @data ||= ARGF.readlines
-      end
+class SyntaxScoring
+  TOKENS      = "()[]{}<>".unpack("C*")
+  TOKENGROUPS = TOKENS.each_slice(2).to_a
+  OTOKEN      = TOKENGROUPS.map(&:min)
+  CTOKEN      = TOKENGROUPS.map(&:max)
+  POINTS      = CTOKEN.zip([3, 57, 1197, 25137]).to_a.to_h
+
+  def part1
+    data
+      .filter { _1[:type].eql?(:invalid) }
+      .sum    { POINTS[_1[:code][0]] }
+  end
+
+  def part2
+    scores = data
+      .select    { _1[:type].eql?(:incomplete) }
+      .map       { _1[:callstack].reverse.reduce(0) { |acc, token| (acc * 5) + OTOKEN.index(token).next } }
+      .sort
+
+    scores[scores.count/2]
+  end
+
+  private
+
+  def reduceindexpoints(linemeta)
+    linemeta[:callstack].reduce(0) do |acc, token| 
+      acc * 5 + (CTOKEN.index(token) || OTOKEN.index(token)).next 
     end
+  end
+
+  def parse(callstack, code)
+    return { type: :valid, callstack: callstack, code: code} if code.empty? && callstack.empty?
+    return { type: :incomplete, callstack: callstack, code: code } if code.empty? && !callstack.empty?
+
+    if [1, 2].map { _1 + callstack[-1].to_i }.include?(code[0])
+      parse(callstack.tap(&:pop), code.tap(&:shift)) 
+    elsif OTOKEN.include?(code[0])
+      parse(callstack.push(code.shift), code) 
+    else
+      { type: :invalid, callstack: callstack, code: code }
+    end
+  end
+
+  def data
+    @data ||= ARGF.readlines.map(&:strip).map { _1.unpack("C*") }.map { parse([], _1) }
+  end
+end
+
+solver = SyntaxScoring.new
+pp solver.part1
+pp solver.part2

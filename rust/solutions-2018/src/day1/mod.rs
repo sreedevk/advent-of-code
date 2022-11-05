82c17cf9dcb9882b760030920362ad505a29776e
sreedev <sreedevpadmakumar@gmail.com>
2022-11-05 04:07:30 -0400

    added few more solutions

---
+++ rust/solutions-2018/src/day1/mod.rs
@@ -0,0 +1,8 @@
+pub struct Day1;
+use common::Solution;
+
+impl Solution for Day1 {
+    fn solve() -> [String; 2] {
+        [String::from("test"), String::from("test")] 
+    }
+}
---
+++ rust/solutions-2021/src/day6/mod.rs
@@ -0,0 +1,39 @@
+use std::{fs, collections::HashMap};
+use common::Solution;
+
+pub struct Day6;
+type FishCounter = HashMap<u16, usize>;
+
+impl Solution for Day6 {
+    fn solve() -> [String; 2] {
+        [
+            Self::solve1(),
+            String::from("2021")
+        ] 
+    }
+}
+
+impl Day6 {
+    fn tally(fishes: Vec<u16>) -> HashMap<u16, usize> {
+        fishes
+            .into_iter()
+            .fold(FishCounter::new(), |mut c, item| {
+                c.entry(item).and_modify(|x| *x += 1 ).or_insert(1);
+                c
+            })
+    }
+
+    fn solve1() -> String {
+        let data = fs::read_to_string("data/example/2021/day6.txt").expect("file not found!");
+        let fishes = data
+            .trim()
+            .split(",")
+            .map(|x| x.trim() )
+            .map(|x| u16::from_str_radix(x, 10).unwrap() ) 
+            .collect::<Vec<u16>>();
+        
+        dbg!(Self::tally(fishes));
+
+        String::from("test")
+    }
+}

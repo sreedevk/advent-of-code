673583122d48e83c2fbdeca7636c444e8f995a25
sreedev <sreedevpadmakumar@gmail.com>
2022-11-13 16:26:49 -0500

    rust: 2021 day 15 - part 1 (build graph)

--- rust/solutions-2021/src/day15/mod.rs
+++ rust/solutions-2021/src/day15/mod.rs
@@ -1,23 +1,69 @@
 mod part1;
 mod part2;
-
 use std::fs;
 
 pub struct Day15;
 
-pub fn build_graph() {
-    let raw_data = fs::read_to_string("data/examples/2021/day15.txt").expect("file_not_found");
-    let map: Vec<Vec<u64>> = raw_data
+#[derive(Clone, Debug)]
+pub struct Cell(usize, usize);
+
+#[derive(Clone, Debug)]
+pub struct Edge(Cell, Cell, usize);
+
+#[derive(Clone, Debug)]
+pub struct Graph {
+    pub vertices: Vec<Cell>,
+    pub edges: Vec<Edge>,
+}
+
+pub fn neighbors(cell: &Cell, map: &Vec<Vec<usize>>) -> Vec<Cell> {
+    let mut nbrs: Vec<Cell> = Vec::with_capacity(9);
+    if !(cell.1 >= map.len() - 1) {
+        nbrs.push(Cell(cell.0, cell.1 + 1));
+    }
+    if !(cell.1 <= 0) {
+        nbrs.push(Cell(cell.0, cell.1 - 1));
+    }
+    if !(cell.0 <= 0) {
+        nbrs.push(Cell(cell.0 - 1, cell.1));
+    }
+    if !(cell.0 >= map.first().unwrap().len() - 1) {
+        nbrs.push(Cell(cell.0 + 1, cell.1));
+    }
+
+    nbrs
+}
+
+pub fn build_graph() -> Graph {
+    let raw_data = fs::read_to_string("data/example/2021/day15.txt").expect("file_not_found");
+    let map: Vec<Vec<usize>> = raw_data
         .trim()
         .split("\n")
         .map(|line| {
             line.chars()
-                .map(|x| u64::from_str_radix(&x.to_string(), 10).unwrap_or_default())
+                .map(|x| usize::from_str_radix(&x.to_string(), 10).unwrap_or_default())
                 .collect()
         })
         .collect();
 
-    dbg!(map);
+    let mut graph = Graph {
+        vertices: vec![],
+        edges: vec![],
+    };
+
+    for (y, row) in map.iter().enumerate() {
+        for (x, _col) in row.iter().enumerate() {
+            let cell = Cell(x, y);
+            graph.vertices.push(cell.clone());
+            let neighboring_cells = neighbors(&cell, &map);
+            for neighboring_cell in neighboring_cells {
+                let edge_val = map[neighboring_cell.1][neighboring_cell.1];
+                graph.edges.push(Edge(cell.clone(), neighboring_cell, edge_val));
+            }
+        }
+    }
+
+    graph
 }
 
 impl Day15 {

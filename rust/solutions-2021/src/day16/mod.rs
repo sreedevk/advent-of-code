522601dce85495466cbc6041e721ab3d6bfb3f95
sreedev <sreedevpadmakumar@gmail.com>
2022-11-29 21:37:07 -0500

    partial day 16 solution - rust

--- rust/solutions-2021/src/day16/mod.rs
+++ rust/solutions-2021/src/day16/mod.rs
@@ -1,3 +1,6 @@
+mod part1;
+mod part2;
+
 pub struct Day16;
 
 #[derive(Debug)]
@@ -15,57 +18,6 @@ pub struct Packet {
 
 impl Day16 {
     pub fn solve() -> [String; 2] {
-        [Self::solve1(), Self::solve2()]
-    }
-
-    fn parse_version(packets: &String) -> u8 {
-        u8::from_str_radix(&packets[0..3], 2).unwrap()
-    }
-
-    fn parse_type(packets: &String) -> PacketType {
-        let raw_type = u8::from_str_radix(&packets[3..6], 2).unwrap();
-        match raw_type {
-            4 => PacketType::Literal,
-            _ => PacketType::Operation,
-        }
-    }
-
-    fn parse_data(raw_data: &String) -> Vec<u8> {
-        let _raw_packets = raw_data
-            .chars()
-            .collect::<Vec<char>>()
-            .chunks(5)
-            .map(|c| Vec::from(c))
-            .collect::<Vec<Vec<char>>>();
-
-        vec![1, 2, 3]
-    }
-
-    fn parse_packet(raw_bytes: &String) -> Packet {
-        let pkt_binary = raw_bytes
-            .chars()
-            .map(|c| c.to_digit(16).unwrap())
-            .map(|d| format!("{:04b}", d))
-            .fold(String::new(), |acc, s| acc + s.as_str());
-
-        let pkt_version = Self::parse_version(&pkt_binary);
-        let pkt_type = Self::parse_type(&pkt_binary);
-        let pkt_data = Self::parse_data(&String::from(&pkt_binary[6..]));
-
-        Packet {
-            version: pkt_version,
-            ptype: pkt_type,
-            data: pkt_data,
-        }
-    }
-
-    fn solve1() -> String {
-        let input = String::from("D2FE28");
-        let pkt = Self::parse_packet(&input);
-        String::from(format!("{:?}", pkt))
-    }
-
-    fn solve2() -> String {
-        String::from("test")
+        [part1::solve(), part2::solve()]
     }
 }
---
+++ rust/solutions-2021/src/day16/part1.rs
@@ -0,0 +1,82 @@
+use std::fs;
+
+#[derive(Debug)]
+enum Packet {
+    Literal {
+        version: u8,
+        value: usize,
+    },
+    Operator {
+        version: u8,
+        sub_packets: Vec<Packet>,
+    },
+}
+
+pub fn solve() -> String {
+    let raw_bin = to_binary(read_raw());
+    let packet = to_packet(raw_bin);
+    String::from("UNSOLVED")
+}
+
+fn parse_literal(bin: String) -> Packet {
+    let version = u8::from_str_radix(&bin[0..3], 2).unwrap();
+    let data_bits = &bin[6..].chars().collect::<Vec<char>>();
+    let value = parse_data_bits(&data_bits);
+    Packet::Literal { version, value }
+}
+
+fn parse_data_bits(bin: &Vec<char>) -> usize {
+    let mut raw: String = String::new();
+    for nibble in bin.chunks(5) {
+        raw += &String::from_iter(&nibble[1..]);
+
+        if nibble[0] == '0' {
+            break;
+        }
+    }
+
+    usize::from_str_radix(&raw, 2).unwrap()
+}
+
+fn parse_operator(bin: String) -> Packet {
+    let version = u8::from_str_radix(&bin[0..3], 2).unwrap();
+    let length_type_id = &bin[6..7];
+    match length_type_id {
+        "0" => {
+            let sub_packet_size = usize::from_str_radix(&bin[7..22], 2).unwrap();
+            let raw_sub_packets = &bin[22..(22 + sub_packet_size)];
+            let sub_packets = to_packet()
+        }
+        "1" => {}
+        _ => panic!("INVALID LENGTH TYPE ID"),
+    }
+    dbg!(length_type_id);
+    Packet::Operator {
+        version,
+        sub_packets: vec![],
+    }
+}
+
+fn to_packet(bin: String) -> Packet {
+    let packet_type = u8::from_str_radix(&bin[3..6], 2).unwrap();
+    match packet_type {
+        4 => parse_literal(bin),
+        _ => parse_operator(bin),
+    }
+}
+
+fn to_binary(raw: String) -> String {
+    raw.chars().into_iter().fold(String::new(), |ostr, chr| {
+        let cstr = chr.to_string();
+        let decimal = u8::from_str_radix(&cstr, 16).unwrap();
+        ostr + &format!("{:04b}", decimal)
+    })
+}
+
+fn read_raw() -> String {
+    return String::from("38006F45291200");
+    fs::read_to_string("data/example/2021/day16.txt")
+        .unwrap()
+        .trim()
+        .to_string()
+}

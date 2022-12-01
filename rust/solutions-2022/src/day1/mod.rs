2715e10a8b1f39f1d583ece65104880fc690b0b3
sreedev <sreedevpadmakumar@gmail.com>
2022-12-01 00:23:55 -0500

    added itertools to 2022

--- rust/solutions-2022/src/day1/mod.rs
+++ rust/solutions-2022/src/day1/mod.rs
@@ -1,4 +1,5 @@
 pub struct Day1;
+use itertools::Itertools;
 
 impl Day1 {
     pub fn solve() -> [String; 2] {
@@ -10,26 +11,33 @@ impl Day1 {
 
     fn solve1() -> String {
         let data = std::fs::read_to_string("data/main/2022/day1.txt").unwrap();
-        let elfs = data.trim().split("\n\n").map(|elf| { 
-            elf.split("\n").map(|meal| {
-                usize::from_str_radix(meal.trim(), 10).unwrap()
-            }).fold(0, |count, item| count + item)
-        }).max();
+        let solution = data
+            .trim()
+            .split("\n\n")
+            .map(|elf| { 
+                elf
+                    .split("\n")
+                    .map(|meal| usize::from_str_radix(meal.trim(), 10).unwrap())
+                    .fold(0, |count, item| count + item)
+            })
+            .max();
 
-        String::from(format!("{:?}", elfs))
+        String::from(format!("{:?}", solution.unwrap()))
     }
 
     fn solve2() -> String {
         let data = std::fs::read_to_string("data/main/2022/day1.txt").unwrap();
-        let mut elfs = data.trim().split("\n\n").map(|elf| { 
-            elf.split("\n").map(|meal| {
-                usize::from_str_radix(meal.trim(), 10).unwrap()
-            }).fold(0, |count, item| count + item)
-        }).collect::<Vec<usize>>();
+        let solution = data.trim().split("\n\n").map(|elf| { 
+            elf
+                .split("\n")
+                .map(|meal| usize::from_str_radix(meal.trim(), 10).unwrap())
+                .fold(0, |count, item| count + item)
+        })
+        .sorted()
+        .rev()
+        .take(3)
+        .sum::<usize>();
 
-        elfs.sort();
-        let sum = elfs.into_iter().rev().take(3).sum::<usize>();
-
-        String::from(format!("{:?}", sum))
+        String::from(format!("{:?}", solution))
     }
 }

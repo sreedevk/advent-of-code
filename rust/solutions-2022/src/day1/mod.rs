0ce64f89d630ce9fd1b2f10bd5f7eb89289c5a80
sreedev <sreedevpadmakumar@gmail.com>
2022-12-02 12:29:17 -0500

    speeeeeeeed

--- rust/solutions-2022/src/day1/mod.rs
+++ rust/solutions-2022/src/day1/mod.rs
@@ -3,10 +3,12 @@ use itertools::Itertools;
 
 impl Day1 {
     pub fn solve() -> [String; 2] {
-        [
-            Day1::solve1(),
-            Day1::solve2(),
-        ]
+        let p1 = std::thread::spawn(|| Day1::solve1());
+        let p2 = std::thread::spawn(|| Day1::solve2());
+        let s1 = p1.join();
+        let s2 = p2.join();
+
+        [s1.unwrap(), s2.unwrap()]
     }
 
     fn solve1() -> String {
@@ -14,9 +16,8 @@ impl Day1 {
         let solution = data
             .trim()
             .split("\n\n")
-            .map(|elf| { 
-                elf
-                    .split("\n")
+            .map(|elf| {
+                elf.split("\n")
                     .map(|meal| usize::from_str_radix(meal.trim(), 10).unwrap())
                     .sum::<usize>()
             })
@@ -27,16 +28,18 @@ impl Day1 {
 
     fn solve2() -> String {
         let data = std::fs::read_to_string("data/main/2022/day1.txt").unwrap();
-        let solution = data.trim().split("\n\n").map(|elf| { 
-            elf
-                .split("\n")
-                .map(|meal| usize::from_str_radix(meal.trim(), 10).unwrap())
-                .fold(0, |count, item| count + item)
-        })
-        .sorted()
-        .rev()
-        .take(3)
-        .sum::<usize>();
+        let solution = data
+            .trim()
+            .split("\n\n")
+            .map(|elf| {
+                elf.split("\n")
+                    .map(|meal| usize::from_str_radix(meal.trim(), 10).unwrap())
+                    .fold(0, |count, item| count + item)
+            })
+            .sorted()
+            .rev()
+            .take(3)
+            .sum::<usize>();
 
         String::from(format!("{:?}", solution))
     }

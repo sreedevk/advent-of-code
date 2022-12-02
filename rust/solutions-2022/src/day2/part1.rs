f3b6fd516916a80dd31dfd2a35edfa87f94f90d8
sreedev <sreedevpadmakumar@gmail.com>
2022-12-02 08:54:20 -0500

    2022 day2 part 1 rust - improvements

--- rust/solutions-2022/src/day2/part1.rs
+++ rust/solutions-2022/src/day2/part1.rs
@@ -1,15 +1,24 @@
-use std::fs;
 use std::collections::HashMap;
+use std::fs;
 
 #[derive(PartialEq, Eq, Hash)]
 #[repr(u8)]
 enum Shape {
     Rock = 1,
     Paper = 2,
-    Scissors = 3
+    Scissors = 3,
 }
 
 type WinningStrat = HashMap<Shape, Shape>;
+
+fn str_to_shape(istr: &str) -> Shape {
+    match istr {
+        "A" | "X" => Shape::Rock,
+        "B" | "Y" => Shape::Paper,
+        "C" | "Z" => Shape::Scissors,
+        _ => panic!("INVALID SHAPE"),
+    }
+}
 
 pub fn solve() -> String {
     let data = fs::read_to_string("data/main/2022/day2.txt").unwrap();
@@ -23,31 +32,20 @@ pub fn solve() -> String {
         .split("\n")
         .map(|t| t.trim())
         .map(|shapes| shapes.split_once(" ").unwrap())
-        .map(|(p0, p1)| {
-            let p0n = match p0 {
-                "A" => Shape::Rock,
-                "B" => Shape::Paper,
-                "C" => Shape::Scissors,
-                _ => panic!("INVALID SIGN")
-            };
-            let p1n = match p1 {
-                "X" => Shape::Rock,
-                "Y" => Shape::Paper,
-                "Z" => Shape::Scissors,
-                _ => panic!("INVALID SIGN")
-            };
-
-            (p0n, p1n)
-        })
-        .map(|shapes| { score(shapes, &win) })
+        .map(|(p0, p1)| (str_to_shape(p0), str_to_shape(p1)))
+        .map(|shapes| score(shapes, &win))
         .sum::<usize>();
 
     String::from(format!("{:?}", score))
 }
 
 fn score((p1, p2): (Shape, Shape), winstrat: &WinningStrat) -> usize {
-    if winstrat.get(&p1).unwrap() == &p2 { return p2 as usize }
-    if p1 == p2 { return (p2 as usize) + 3 }
+    if winstrat.get(&p1).unwrap() == &p2 {
+        return p2 as usize;
+    }
+    if p1 == p2 {
+        return (p2 as usize) + 3;
+    }
 
     p2 as usize + 6
 }

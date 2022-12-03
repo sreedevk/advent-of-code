5293e5cb508ef74e8fc3a65cfcc338a206acb6b6
sreedev <sreedevpadmakumar@gmail.com>
2022-12-03 00:44:28 -0500

    2022 day 3 - rust

--- rust/solutions-2022/src/day3/part1.rs
+++ rust/solutions-2022/src/day3/part1.rs
@@ -1,3 +1,32 @@
+use std::{collections::HashSet, fs};
+use itertools::Itertools;
+
+fn to_priority(input: char) -> usize {
+    if input.is_uppercase() {
+        (input as usize) - 38
+    } else {
+        (input as usize) - 96
+    }
+}
+
+fn intersection(a: Vec<usize>, b: Vec<usize>) -> usize {
+    a.into_iter().find(|x| b.contains(x)).unwrap()
+}
+
 pub fn solve() -> String {
-    String::from("UNSOLVED")
+    let raw = fs::read_to_string("data/main/2022/day3.txt").unwrap();
+    let priorities_sum = raw
+        .trim()
+        .split('\n')
+        .into_iter()
+        .map(|sack| sack.trim().chars().map(to_priority).collect::<Vec<usize>>())
+        .collect::<Vec<Vec<usize>>>()
+        .into_iter()
+        .map(|x| {
+            let (a, b) = x.split_at(x.len() / 2);
+            intersection(a.to_owned(), b.to_owned())
+        })
+        .sum::<usize>();
+
+        format!("{}", priorities_sum)
 }
--- rust/solutions-2022/src/day3/part2.rs
+++ rust/solutions-2022/src/day3/part2.rs
@@ -1,3 +1,37 @@
+use itertools::Itertools;
+use std::{collections::HashSet, fs};
+
+fn to_priority(input: char) -> usize {
+    if input.is_uppercase() {
+        (input as usize) - 38
+    } else {
+        (input as usize) - 96
+    }
+}
+
+fn intersection(a: Vec<usize>, b: Vec<usize>) -> usize {
+    a.into_iter().find(|x| b.contains(x)).unwrap()
+}
+
+fn three_way(a: Vec<usize>, b: Vec<usize>, c: Vec<usize>) -> usize {
+    a.into_iter()
+        .find(|x| b.contains(x) && c.contains(x))
+        .unwrap()
+}
+
 pub fn solve() -> String {
-    String::from("UNSOLVED")
+    let raw = fs::read_to_string("data/main/2022/day3.txt").unwrap();
+    let priorities_sum = raw
+        .trim()
+        .split('\n')
+        .map(|sack| sack.trim().chars().map(to_priority).collect::<Vec<usize>>())
+        .chunks(3)
+        .into_iter()
+        .map(|chunk| {
+            let elfchunk = chunk.collect_vec();
+            three_way(elfchunk[0].to_owned(), elfchunk[1].to_owned(), elfchunk[2].to_owned())
+        })
+        .sum::<usize>();
+
+    format!("{}", priorities_sum)
 }

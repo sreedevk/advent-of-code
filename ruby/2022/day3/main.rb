c81f7fff0cf95b2481b4e39290f07c3211208d02
sreedev <sreedevpadmakumar@gmail.com>
2022-12-03 02:38:06 -0500

    ruby optimizations

--- ruby/2022/day3/main.rb
+++ ruby/2022/day3/main.rb
@@ -1,26 +1,30 @@
 # frozen_string_literal: true
+require 'set'
 
 # Day 3 2022
 class RucksackReorganization
+  UPPER = Set.new('A'..'Z')
+
   def initialize(data)
     @data = data
+            .lazy
             .map(&:strip)
             .map(&:chars)
   end
 
   def solve1
     @data
-      .map { |x| x.each_slice(x.size / 2) }
-      .map { |x| x.reduce(&:&).first }
-      .map { |x| x.ord - (/[[:upper:]]/ =~ x ? 38 : 96) }
+      .map { _1.each_slice(_1.size / 2) }
+      .map { _1.reduce(&:&).first }
+      .map { _1.ord - (UPPER.member?(_1) ? 38 : 96) }
       .sum
   end
 
   def solve2
     @data
       .each_slice(3)
-      .map { |slice| slice.reduce(&:&).first }
-      .map { |x| x.ord - (/[[:upper:]]/ =~ x ? 38 : 96) }
+      .map { _1.reduce(&:&).first }
+      .map { _1.ord - (UPPER.member?(_1) ? 38 : 96) }
       .sum
   end
 end

6d26c4df3be468f4d6a3177c9f303a3871111971
sreedev <sreedevpadmakumar@gmail.com>
2022-12-04 07:46:39 -0500

    code opts

--- rust/solutions-2022/src/day4/mod.rs
+++ rust/solutions-2022/src/day4/mod.rs
@@ -5,9 +5,6 @@ pub struct Day4;
 
 impl Day4 {
     pub fn solve() -> [String; 2] {
-        [
-            part1::solve(),
-            part2::solve()
-        ]
+        [part1::solve(), part2::solve()]
     }
 }

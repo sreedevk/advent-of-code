9ddd2ca47348a28bc43455a395c92c34629b01f1
sreedev <sreedevpadmakumar@gmail.com>
2022-12-06 00:17:29 -0500

    day 6 2022: rust

--- rust/solutions-2022/src/day6/part1.rs
+++ rust/solutions-2022/src/day6/part1.rs
@@ -1,3 +1,20 @@
+use std::fs;
+use itertools::Itertools;
+
+fn all_unique(chrs: Vec<char>) -> bool {
+    chrs == chrs.clone().into_iter().unique().collect_vec()
+}
+
 pub fn solve() -> String {
-    String::from("test")
+    let raw = fs::read_to_string("data/main/2022/day6.txt").unwrap();
+    let results = raw
+        .chars()
+        .collect_vec()
+        .windows(4)
+        .into_iter()
+        .enumerate()
+        .filter_map(|(index, chars)| { if all_unique(chars.to_vec()) { Some(index + 4) } else { None } })
+        .collect_vec();
+
+    format!("{}", results.get(0).unwrap())
 }
--- rust/solutions-2022/src/day6/part2.rs
+++ rust/solutions-2022/src/day6/part2.rs
@@ -1,3 +1,21 @@
-pub fn solve() -> String {
-    String::from("test")
+use std::fs;
+use itertools::Itertools;
+
+fn all_unique(chrs: Vec<char>) -> bool {
+    chrs == chrs.clone().into_iter().unique().collect_vec()
 }
+
+pub fn solve() -> String {
+    let raw = fs::read_to_string("data/main/2022/day6.txt").unwrap();
+    let results = raw
+        .chars()
+        .collect_vec()
+        .windows(14)
+        .into_iter()
+        .enumerate()
+        .filter_map(|(index, chars)| { if all_unique(chars.to_vec()) { Some(index + 14) } else { None } })
+        .collect_vec();
+
+    format!("{}", results.get(0).unwrap())
+}
+

cae694a2d3f17b456097cfafc26e08b0a92d4c61
sreedev <sreedevpadmakumar@gmail.com>
2022-12-06 23:24:06 -0500

    codeopts day 6 2022

--- rust/solutions-2021/src/day6/part2.rs
+++ rust/solutions-2021/src/day6/part2.rs
@@ -1,45 +1,43 @@
 use super::FishCounter;
 use itertools::Itertools;
 use std::fs;
+use tap::prelude::*;
 
 fn step_gen(state: FishCounter, max_gens: usize, current_gen: usize) -> FishCounter {
-    let finstate = state
-        .into_iter()
-        .fold(FishCounter::new(), |mut new_state, (age, count)| {
-            if age == 0 {
-                new_state
-                    .entry(6)
-                    .and_modify(|ct| *ct += count)
-                    .or_insert(count);
-                new_state
-                    .entry(8)
-                    .and_modify(|ct| *ct += count)
-                    .or_insert(count);
-                new_state
-            } else {
-                new_state
-                    .entry(age - 1)
-                    .and_modify(|ct| *ct += count)
-                    .or_insert(count);
-                new_state
-            }
-        });
-
-    if current_gen >= max_gens {
-        finstate
-    } else {
-        step_gen(finstate, max_gens, current_gen + 1)
+    if current_gen > max_gens {
+        return state;
     }
+    state
+        .into_iter()
+        .fold(FishCounter::new(), |new_state, (age, count)| {
+            new_state
+                .tap_mut(|x| {
+                    if age == 0 {
+                        x.entry(6).and_modify(|ct| *ct += count).or_insert(count);
+                        x.entry(8).and_modify(|ct| *ct += count).or_insert(count);
+                    }
+                })
+                .tap_mut(|x| {
+                    if age != 0 {
+                        x.entry(age - 1)
+                            .and_modify(|ct| *ct += count)
+                            .or_insert(count);
+                    }
+                })
+        })
+        .pipe(|next_gen| step_gen(next_gen, max_gens, current_gen + 1))
 }
 
 fn tally(input: Vec<usize>) -> FishCounter {
     input
         .into_iter()
-        .fold(FishCounter::new(), |mut ctr: FishCounter, num| {
-            ctr.entry(num)
-                .and_modify(|counter| *counter += 1)
-                .or_insert(1);
-            ctr
+        .fold(FishCounter::new(), |ctr: FishCounter, num| {
+            ctr.tap_mut(|counter| {
+                counter
+                    .entry(num)
+                    .and_modify(|counter| *counter += 1)
+                    .or_insert(1);
+            })
         })
 }
 
@@ -52,11 +50,11 @@ fn parse_input(input: &str) -> Vec<usize> {
 }
 
 pub fn solve() -> String {
-    let input = fs::read_to_string("data/main/2021/day6.txt").unwrap();
-    let final_gen = step_gen(tally(parse_input(input.as_str())), 256, 1)
-        .values()
-        .sum::<usize>();
-
-    format!("{}", final_gen)
+    fs::read_to_string("data/main/2021/day6.txt")
+        .unwrap()
+        .pipe(|input| parse_input(input.as_str()))
+        .pipe(|input| tally(input))
+        .pipe(|input| step_gen(input, 256, 1))
+        .pipe(|input| input.values().sum::<usize>())
+        .pipe(|solution| format!("{}", solution))
 }
-
